ad55967f877dfdfe545393dddb6457eb025b258d
fikma <ade.fm97@gmail.com>
2022-10-15 07:31:34 +0700

    CounterObserver
    Berfungsi untuk meng-observer/ mengamati?? counter state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:fatree/counter_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = CounterObserver();
+
   runApp(const Text("Hello world"));
 }
